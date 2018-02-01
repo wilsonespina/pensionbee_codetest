30f0a4e915270f1e2d16fdb151934dc43a1405eb
wilsonespina <wilson.espina@gmail.com>
2018-02-01 17:40:43 +0000

    adding inline styling

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const port = process.env.PORT || 7000;
+// const morgan = require('morgan');
+// const bodyParser = require('body-parser');
 const docserver = require('docserver');
 
+const port = process.env.PORT || 7000;
 const app = express();
 
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(morgan('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(docserver({
   dir: __dirname + '/content',
   url: '/content/'}
 ));
 
-app.use(express.static(`${__dirname}`));
-app.get('/*', (req, res) => res.sendFile(`${__dirname}/content/template.html`));
+app.get('/*', (req, res) => res.redirect('/content/about-page'));
 
 app.listen(port, () => console.log(`Started on port: ${port}`));
 
