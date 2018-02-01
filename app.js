7b749287fe2d26f6a424ceaed5752b7911401fef
wilsonespina <wilson.espina@gmail.com>
2018-02-01 16:36:37 +0000

    now using docserver dependency

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const docserver = require('docserver');
 const port = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(`${__dirname}`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/*', (req, res) => res.sendFile(`${__dirname}/index.html`));
+
+app.use(docserver({
+  dir: __dirname + '/content',  // serve Markdown files in the docs directory...
+  url: '/content/'}             // ...and serve them at the root of the site
+));
+
+// app.use(express.static(`${__dirname}`));
+// app.get('/*', (req, res) => res.sendFile(`${__dirname}/template.html`));
 
 app.listen(port, () => console.log(`Started on port: ${port}`));
 
